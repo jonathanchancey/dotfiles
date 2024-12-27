5ad348b9d8700811e5a2f82fcb1999d35ec42ad1
jonathanchancey <jonathan@chancey.dev>
2024-12-27 13:48:17 -0800

    feat(neovim): add catppuccin theme

--- roles/neovim/files/nvim/lua/community.lua
+++ roles/neovim/files/nvim/lua/community.lua
@@ -7,5 +7,6 @@ return {
 	"AstroNvim/astrocommunity",
 	{ import = "astrocommunity.pack.lua" },
 	{ import = "astrocommunity.colorscheme.kanagawa-nvim" },
+	{ import = "astrocommunity.colorscheme.catppuccin" },
 	-- import/override with your plugins folder
 }
