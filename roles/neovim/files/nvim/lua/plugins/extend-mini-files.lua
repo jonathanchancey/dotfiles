f53b70b431308ca2468df6c84090513d2fe38b55
Jonathan Chancey <jonathan@chancey.dev>
2025-03-29 17:30:11 -0700

    feat: add python venv

--- roles/neovim/files/nvim/lua/plugins/extend-mini-files.lua
+++ roles/neovim/files/nvim/lua/plugins/extend-mini-files.lua
@@ -7,7 +7,7 @@ return {
       function()
         require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
       end,
-      desc = "Open mini.files at directory of current file",
+      desc = "Open mini.files at current file",
     },
     {
       "<leader>E",
