b20705f7ce423730468f3d2e2e9cee554dfaaf9d
Jonathan Chancey <jonathan@chancey.dev>
2025-04-14 16:41:02 -0700

    feat(nvim): add new file and save keybinds

--- roles/neovim/files/nvim/lua/config/keymaps.lua
+++ roles/neovim/files/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>wN", "<cmd>enew<cr>", { desc = "New File in Current Window" })
+vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save" })
+vim.keymap.set("i", "<D-s>", "<Esc><cmd>w<cr>a", { desc = "Save" })
