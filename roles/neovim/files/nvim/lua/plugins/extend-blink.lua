695f8e9cf6308f7a3b58ed1dac693f1688786aa4
Jonathan Chancey <jonathan@chancey.dev>
2025-05-14 14:39:13 -0700

    feat(nvim): disable blink.cmp preselect

--- roles/neovim/files/nvim/lua/plugins/extend-blink.lua
+++ roles/neovim/files/nvim/lua/plugins/extend-blink.lua
@@ -1,6 +1,8 @@
--- return {
---   "saghen/blink.cmp",
---   opts = {
---     list = { selection = { preselect = false, auto_insert = true } },
---   },
--- }
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = { selection = { preselect = false } },
+    },
+  },
+}
