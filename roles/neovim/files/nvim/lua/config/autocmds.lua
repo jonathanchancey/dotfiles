b384e269460188465a40593550da476eebbed61b
Jonathan Chancey <jonathan@chancey.dev>
2025-06-16 16:44:24 -0700

    fix(nvim): update nvim terminal mode switch

--- roles/neovim/files/nvim/lua/config/autocmds.lua
+++ roles/neovim/files/nvim/lua/config/autocmds.lua
@@ -7,4 +7,4 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]])
