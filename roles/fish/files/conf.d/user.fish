80d16cfed1e518ae7e61370c17a9b63b21fb1bb0
Jonathan Chancey <jonathan@chancey.dev>
2025-10-08 10:01:55 -0700

    feat(fish): remove vim binds

--- roles/fish/files/conf.d/user.fish
+++ roles/fish/files/conf.d/user.fish
@@ -53,17 +53,6 @@ if status is-interactive
     set pure_enable_single_line_prompt false
     set pure_enable_k8s true
 
-    # function fish_user_key_bindings
-    #     # Execute this once per mode that emacs bindings should be used in
-    #     fish_default_key_bindings -M insert
-    #
-    #     # Then execute the vi-bindings so they take precedence when there's a conflict.
-    #     # Without --no-erase fish_vi_key_bindings will default to
-    #     # resetting all bindings.
-    #     # The argument specifies the initial mode (insert, "default" or visual).
-    #     fish_vi_key_bindings --no-erase insert
-    # end
-
     # kubectl completion fish | source
     zoxide init fish | source
 
