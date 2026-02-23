0ef597b4a58dc68952d5d4a90541b6c05397532c
Jonathan Chancey <jonathan@chancey.dev>
2026-02-23 12:41:32 -0800

    feat(nvim): add jinja surround
    and remove other unused custom entries

--- roles/neovim/files/nvim/lua/plugins/extend-mini-surround.lua
+++ roles/neovim/files/nvim/lua/plugins/extend-mini-surround.lua
@@ -2,13 +2,9 @@ return {
   "nvim-mini/mini.surround",
   opts = {
     custom_surroundings = {
-      ["d"] = {
-        input = { "'''().-()'''" }, -- Added capture groups
-        output = { left = "'''\n", right = "\n'''" },
-      },
-      ["`"] = {
-        input = { "```&quot;, &quot;```" },
-        output = { left = "```\n&quot;, right = &quot;\n```" },
+      j = {
+        input = { "%{%{%s*().-()%s*%}%}" },
+        output = { left = "{{ ", right = " }}" },
       },
     },
   },
