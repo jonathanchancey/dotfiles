33d34e6a86dafb090eb0628e0e3cab6b10527c52
Jonathan Chancey <jonathan@chancey.dev>
2026-05-07 15:49:09 -0700

    feat(nvim): add obsidian-style toggle markdown checkbox keybind

--- roles/neovim/files/nvim/lua/config/keymaps.lua
+++ roles/neovim/files/nvim/lua/config/keymaps.lua
@@ -5,6 +5,21 @@ vim.keymap.set("n", "<leader>wN", "<cmd>enew<cr>", { desc = "New File in Current
 vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save" })
 vim.keymap.set("i", "<D-s>", "<Esc><cmd>w<cr>a", { desc = "Save" })
 
+local function toggle_markdown_checkbox()
+  local line = vim.api.nvim_get_current_line()
+  local prefix, checked, text = line:match("^(%s*[-*+]%s+)%[([ xX])%](.*)$")
+
+  if prefix then
+    vim.api.nvim_set_current_line(prefix .. (checked == " " and "[x]" or "[ ]") .. text)
+  else
+    vim.api.nvim_set_current_line((line:gsub("^(%s*)", "%1- [ ] ", 1)))
+  end
+end
+
+vim.keymap.set("n", "<D-l>", function()
+  toggle_markdown_checkbox()
+end, { desc = "Toggle Markdown Checkbox" })
+
 vim.keymap.set("n", "<leader>fy", function()
   vim.fn.setreg(vim.v.register, vim.fn.expand("%:p"))
 end, { desc = "Copy filename+line to clipboard" })
